f1b957c971ad508a07ff5a026e3710b067b0cecb
pamcuellas <pamcuellas@gmail.com>
2020-11-01 15:57:24 -0500

    Style map changed.

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,7 +10,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
 tileSize: 512,
 maxZoom: 18,
-id: "mapbox.streets",
+id: "satellite-streets-v9",
 accessToken: "pk.eyJ1IjoicGFtY3VlbGxhcyIsImEiOiJjazB0bXAyd2kwZjR0M2xyenR4c3pxMnNwIn0.cYcsaNRZ95uSPxGLGm85rw"
 }).addTo(myMap);
 
