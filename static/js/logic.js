7deec1ca3efb1cf26113faad2e22529661a3aa15
pamcuellas <pamcuellas@gmail.com>
2020-11-01 16:23:31 -0500

    Style map changed.

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,7 +5,7 @@ var myMap = L.map("map", {
 });
 
 // Add a tile layer
-
+/*
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
     tileSize: 512,
@@ -14,6 +14,20 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v11',
     accessToken: "pk.eyJ1IjoicGFtY3VlbGxhcyIsImEiOiJjazB0bXAyd2kwZjR0M2xyenR4c3pxMnNwIn0.cYcsaNRZ95uSPxGLGm85rw"
 }).addTo( myMap );
+*/
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicGFtY3VlbGxhcyIsImEiOiJjazB0bXAyd2kwZjR0M2xyenR4c3pxMnNwIn0.cYcsaNRZ95uSPxGLGm85rw'
+}).addTo( myMap );
+
+
+
+
+
 
 /*
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
