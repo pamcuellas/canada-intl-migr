f75f2c177ac4cda18d8da3b07b2916b54b26c7b9
pamcuellas <pamcuellas@gmail.com>
2020-11-01 16:02:54 -0500

    Style map changed.

--- static/js/logic.js
+++ static/js/logic.js
@@ -6,13 +6,14 @@ var myMap = L.map("map", {
 
 // Add a tile layer
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-attribution: '© <a href="https://www.mapbox.com/about/maps/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> <strong><a href="https://www.mapbox.com/map-feedback/" target="_blank">Improve this map</a></strong>',
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 tileSize: 512,
 maxZoom: 18,
 id: "satellite-streets-v9",
 accessToken: "pk.eyJ1IjoicGFtY3VlbGxhcyIsImEiOiJjazB0bXAyd2kwZjR0M2xyenR4c3pxMnNwIn0.cYcsaNRZ95uSPxGLGm85rw"
 }).addTo(myMap);
+
 
 /*
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
